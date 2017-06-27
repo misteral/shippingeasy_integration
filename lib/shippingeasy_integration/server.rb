c6fe350a70a35121158a8584b5df07193d244d7f
Aleksanr Bobrov <al.bobrov.mail@gmail.com>
2017-06-27 18:35:04 +0800

    Fixes for shipping easy

--- lib/shippingeasy_integration/server.rb
+++ lib/shippingeasy_integration/server.rb
@@ -6,7 +6,7 @@ require 'logger'
 
 module ShippingeasyIntegration
   class Server < EndpointBase::Sinatra::Base
-    before ['/cancel_order', '/create_order'] do
+    before ['/cancel_order', '/create_order', '/update_order'] do
       logger.info "Config=#{@config}"
       logger.info "Payload=#{@payload}"
 
@@ -31,17 +31,26 @@ module ShippingeasyIntegration
 
     post '/update_order' do
       begin
-        order = ShippingEasy::Resources::Order.find(id: @payload[:shipping_easy][:order][:sync_id])
+        # find order
+        order =
+          ShippingEasy::Resources::Order
+          .find(
+            id: @payload[:shipping_easy][:order][:sync_id]
+          )
 
+        # cancel order
         ShippingEasy::Resources::Cancellation
           .create(store_api_key: @config['store_api_key'],
                   external_order_identifier: \
-          @payload[:shipping_easy][:order][:external_order_identifier])
+          order['order']['external_order_identifier'])
+
+        # create order
         new_identifier = modify_indentifier(order['order']['external_order_identifier'])
         @payload[:shipping_easy][:order][:external_order_identifier] = new_identifier
         new_order = ShippingEasy::Resources::Order
                     .create(store_api_key: @config['store_api_key'],
                             payload: @payload[:shipping_easy])
+
         # response part
         add_object :order, id: demodify_identyfier(new_order['order']['external_order_identifier']),
                            sync_id: new_order['order']['id'], sync_type: 'shipping_easy'
@@ -55,10 +64,27 @@ module ShippingeasyIntegration
 
     post '/cancel_order' do
       begin
-        response = ShippingEasy::Resources::Cancellation
-                   .create(store_api_key: @config['store_api_key'],
-                           external_order_identifier: \
-                             @payload[:shipping_easy][:order][:external_order_identifier])
+        if @payload[:shipping_easy][:order][:sync_id]
+          # find order
+          order =
+            ShippingEasy::Resources::Order
+            .find(
+              id: @payload[:shipping_easy][:order][:sync_id]
+            )
+
+          # cancel order
+          response =
+            ShippingEasy::Resources::Cancellation
+            .create(store_api_key: @config['store_api_key'],
+                    external_order_identifier: \
+              order['order']['external_order_identifier'])
+        else
+          response =
+            ShippingEasy::Resources::Cancellation
+            .create(store_api_key: @config['store_api_key'],
+                    external_order_identifier: \
+              @payload[:shipping_easy][:order][:external_order_identifier])
+        end
 
         logger.info "Response from Shipping Easy = #{response}"
         result 200, 'Order with is canceled from Shipping Easy.'
@@ -95,7 +121,7 @@ module ShippingeasyIntegration
     def modify_indentifier(order_number)
       return "#{order_number}_1" if order_number.partition('_').last.empty?
       order_number_prefix = order_number.partition('_').last.to_i + 1
-      "#{order_number}_#{order_number_prefix}"
+      "#{demodify_identyfier(order_number)}_#{order_number_prefix}"
     end
 
     def demodify_identyfier(modified_number)
